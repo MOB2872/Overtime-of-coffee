cd967ffee82447ff5177c5f4a979400400e09730
MOB2872 <145713719+MOB2872@users.noreply.github.com>
2024-10-22 14:37:22 -0300

    Add files via upload

---
+++ cafeteria/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Definindo a rota principal do site 
+@app.route('/')
+def home ():
+    return render_template('index.html')
+
+@app.route('/cafes')
+def cafes ():
+    return render_template('cafes.html')
+
+# Parte principal do programa em Python
+if __name__ == '__main__':
+    app.run(debug=True) 
